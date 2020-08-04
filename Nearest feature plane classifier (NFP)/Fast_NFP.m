dd47cc88177df3efc3fcb8712e43918b11921c4e
guijiejie <46763656+guijiejie@users.noreply.github.com>
2020-08-04 17:16:12 -0400

    Create Fast_NFP.m

---
+++ Nearest feature plane classifier (NFP)/Fast_NFP.m
@@ -0,0 +1,57 @@
+function rate = Fast_NFP(Train_data,Train_label,Test_data,Test_label,ratio);
+% Fast Nearest Feature Plane classifier(NFP classifier)
+% For the datasets such as Action recognition database ASLAN, the number of
+% each class is very large. The code 'ttt=combntns(1:length_index,3)' or
+% 'ttt=combntns(1:length_index,2)' is out of memory. Our code randomly
+% selects some combinations.
+% The smaller ratio is, the faster our code is.
+%Input:
+%     Train_data,Test_data are training data set and test data
+%     set,respectively.(Each row is a data point)
+%     Train_label,Test_label are column vectors.They are labels of training
+%     data set and test data set,respectively.
+%Output:
+%     rate:Accuracy of NFP classifier
+%This code is written by Gui Jie on the morning of 12/13/2012.
+%If you have find some bugs in the codes, feel free to contract me
+
+[n dim]    = size(Test_data);% number of test data set
+train_num  = size(Train_data, 1); % number of training data set
+
+% Normalize each feature to have zero mean and unit variance.
+% If you need the following four rows,you can uncomment them.
+% M        = mean(Train_data); % mean & std of the training data set
+% S        = std(Train_data);
+% Train_data = (Train_data - ones(train_num, 1) * M)./(ones(train_num, 1) * S); % normalize training data set
+% Test_data            = (Test_data-ones(n,1)*M)./(ones(n,1)*S); % normalize data
+
+U        = unique(Train_label); % class labels
+nclasses = length(U);%number of classes
+Result  = zeros(n, 1);
+rand('state',0);
+
+for i = 1:n
+    test=Test_data(i,:);
+    dist = repmat(realmax,[nclasses 1]);
+    for k = 1:nclasses
+        index = find(Train_label==U(k));
+        length_index = length(index);
+        temp1 = randperm(length_index);
+        temp2 = temp1(1:floor(length_index*ratio));
+        ttt = combntns(temp2,3);
+        %ttt=combntns(1:length_index,3);
+        combination_num = size(ttt,1);
+        for j = 1:combination_num
+            temp = PointToPlaneDistance(test,Train_data(index(ttt(j,1)),:),Train_data(index(ttt(j,2)),:),Train_data(index(ttt(j,3)),:));
+            if temp < dist(k,1)
+                dist(k,1) = temp;
+            end
+            
+        end
+        % determine the class of the data sample
+        [dummy ind] = min(dist);
+        Result(i) = U(ind);
+    end
+end
+correctnumbers=length(find(Result==Test_label));
+rate=correctnumbers/n;
